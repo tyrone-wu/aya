f27781190677c9b1a9b2b14f3c37f0010ae7286e
tyrone-wu <wudevelops@gmail.com>
2024-09-06 17:42:41 +0000

    aya: remove unwrap and NonZero* in info
    Addresses the feedback from #1007:
    - remove panic from `unwrap` and `expect`
    - Option<NonZero*> => Option<int> with `0` mapping to `None`
    - use `aya_ebpf::binding` in `u32` conversion in prog, link, and attach type for
      more straightforward matching
    
    Refs: #1007

--- aya-obj/src/programs/types.rs
+++ aya-obj/src/programs/types.rs
@@ -1,50 +1,49 @@
 //! Program type bindings.
 
-use crate::{
-    generated::bpf_prog_type::{self, *},
-    InvalidTypeBinding,
-};
+use crate::{generated::bpf_prog_type, InvalidTypeBinding};
 
 impl TryFrom<u32> for bpf_prog_type {
     type Error = InvalidTypeBinding<u32>;
 
     fn try_from(prog_type: u32) -> Result<Self, Self::Error> {
+        use aya_ebpf::bindings::bpf_prog_type as prog_binding;
+        use bpf_prog_type::*;
         Ok(match prog_type {
-            x if x == BPF_PROG_TYPE_UNSPEC as u32 => BPF_PROG_TYPE_UNSPEC,
-            x if x == BPF_PROG_TYPE_SOCKET_FILTER as u32 => BPF_PROG_TYPE_SOCKET_FILTER,
-            x if x == BPF_PROG_TYPE_KPROBE as u32 => BPF_PROG_TYPE_KPROBE,
-            x if x == BPF_PROG_TYPE_SCHED_CLS as u32 => BPF_PROG_TYPE_SCHED_CLS,
-            x if x == BPF_PROG_TYPE_SCHED_ACT as u32 => BPF_PROG_TYPE_SCHED_ACT,
-            x if x == BPF_PROG_TYPE_TRACEPOINT as u32 => BPF_PROG_TYPE_TRACEPOINT,
-            x if x == BPF_PROG_TYPE_XDP as u32 => BPF_PROG_TYPE_XDP,
-            x if x == BPF_PROG_TYPE_PERF_EVENT as u32 => BPF_PROG_TYPE_PERF_EVENT,
-            x if x == BPF_PROG_TYPE_CGROUP_SKB as u32 => BPF_PROG_TYPE_CGROUP_SKB,
-            x if x == BPF_PROG_TYPE_CGROUP_SOCK as u32 => BPF_PROG_TYPE_CGROUP_SOCK,
-            x if x == BPF_PROG_TYPE_LWT_IN as u32 => BPF_PROG_TYPE_LWT_IN,
-            x if x == BPF_PROG_TYPE_LWT_OUT as u32 => BPF_PROG_TYPE_LWT_OUT,
-            x if x == BPF_PROG_TYPE_LWT_XMIT as u32 => BPF_PROG_TYPE_LWT_XMIT,
-            x if x == BPF_PROG_TYPE_SOCK_OPS as u32 => BPF_PROG_TYPE_SOCK_OPS,
-            x if x == BPF_PROG_TYPE_SK_SKB as u32 => BPF_PROG_TYPE_SK_SKB,
-            x if x == BPF_PROG_TYPE_CGROUP_DEVICE as u32 => BPF_PROG_TYPE_CGROUP_DEVICE,
-            x if x == BPF_PROG_TYPE_SK_MSG as u32 => BPF_PROG_TYPE_SK_MSG,
-            x if x == BPF_PROG_TYPE_RAW_TRACEPOINT as u32 => BPF_PROG_TYPE_RAW_TRACEPOINT,
-            x if x == BPF_PROG_TYPE_CGROUP_SOCK_ADDR as u32 => BPF_PROG_TYPE_CGROUP_SOCK_ADDR,
-            x if x == BPF_PROG_TYPE_LWT_SEG6LOCAL as u32 => BPF_PROG_TYPE_LWT_SEG6LOCAL,
-            x if x == BPF_PROG_TYPE_LIRC_MODE2 as u32 => BPF_PROG_TYPE_LIRC_MODE2,
-            x if x == BPF_PROG_TYPE_SK_REUSEPORT as u32 => BPF_PROG_TYPE_SK_REUSEPORT,
-            x if x == BPF_PROG_TYPE_FLOW_DISSECTOR as u32 => BPF_PROG_TYPE_FLOW_DISSECTOR,
-            x if x == BPF_PROG_TYPE_CGROUP_SYSCTL as u32 => BPF_PROG_TYPE_CGROUP_SYSCTL,
-            x if x == BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE as u32 => {
+            prog_binding::BPF_PROG_TYPE_UNSPEC => BPF_PROG_TYPE_UNSPEC,
+            prog_binding::BPF_PROG_TYPE_SOCKET_FILTER => BPF_PROG_TYPE_SOCKET_FILTER,
+            prog_binding::BPF_PROG_TYPE_KPROBE => BPF_PROG_TYPE_KPROBE,
+            prog_binding::BPF_PROG_TYPE_SCHED_CLS => BPF_PROG_TYPE_SCHED_CLS,
+            prog_binding::BPF_PROG_TYPE_SCHED_ACT => BPF_PROG_TYPE_SCHED_ACT,
+            prog_binding::BPF_PROG_TYPE_TRACEPOINT => BPF_PROG_TYPE_TRACEPOINT,
+            prog_binding::BPF_PROG_TYPE_XDP => BPF_PROG_TYPE_XDP,
+            prog_binding::BPF_PROG_TYPE_PERF_EVENT => BPF_PROG_TYPE_PERF_EVENT,
+            prog_binding::BPF_PROG_TYPE_CGROUP_SKB => BPF_PROG_TYPE_CGROUP_SKB,
+            prog_binding::BPF_PROG_TYPE_CGROUP_SOCK => BPF_PROG_TYPE_CGROUP_SOCK,
+            prog_binding::BPF_PROG_TYPE_LWT_IN => BPF_PROG_TYPE_LWT_IN,
+            prog_binding::BPF_PROG_TYPE_LWT_OUT => BPF_PROG_TYPE_LWT_OUT,
+            prog_binding::BPF_PROG_TYPE_LWT_XMIT => BPF_PROG_TYPE_LWT_XMIT,
+            prog_binding::BPF_PROG_TYPE_SOCK_OPS => BPF_PROG_TYPE_SOCK_OPS,
+            prog_binding::BPF_PROG_TYPE_SK_SKB => BPF_PROG_TYPE_SK_SKB,
+            prog_binding::BPF_PROG_TYPE_CGROUP_DEVICE => BPF_PROG_TYPE_CGROUP_DEVICE,
+            prog_binding::BPF_PROG_TYPE_SK_MSG => BPF_PROG_TYPE_SK_MSG,
+            prog_binding::BPF_PROG_TYPE_RAW_TRACEPOINT => BPF_PROG_TYPE_RAW_TRACEPOINT,
+            prog_binding::BPF_PROG_TYPE_CGROUP_SOCK_ADDR => BPF_PROG_TYPE_CGROUP_SOCK_ADDR,
+            prog_binding::BPF_PROG_TYPE_LWT_SEG6LOCAL => BPF_PROG_TYPE_LWT_SEG6LOCAL,
+            prog_binding::BPF_PROG_TYPE_LIRC_MODE2 => BPF_PROG_TYPE_LIRC_MODE2,
+            prog_binding::BPF_PROG_TYPE_SK_REUSEPORT => BPF_PROG_TYPE_SK_REUSEPORT,
+            prog_binding::BPF_PROG_TYPE_FLOW_DISSECTOR => BPF_PROG_TYPE_FLOW_DISSECTOR,
+            prog_binding::BPF_PROG_TYPE_CGROUP_SYSCTL => BPF_PROG_TYPE_CGROUP_SYSCTL,
+            prog_binding::BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE => {
                 BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE
             }
-            x if x == BPF_PROG_TYPE_CGROUP_SOCKOPT as u32 => BPF_PROG_TYPE_CGROUP_SOCKOPT,
-            x if x == BPF_PROG_TYPE_TRACING as u32 => BPF_PROG_TYPE_TRACING,
-            x if x == BPF_PROG_TYPE_STRUCT_OPS as u32 => BPF_PROG_TYPE_STRUCT_OPS,
-            x if x == BPF_PROG_TYPE_EXT as u32 => BPF_PROG_TYPE_EXT,
-            x if x == BPF_PROG_TYPE_LSM as u32 => BPF_PROG_TYPE_LSM,
-            x if x == BPF_PROG_TYPE_SK_LOOKUP as u32 => BPF_PROG_TYPE_SK_LOOKUP,
-            x if x == BPF_PROG_TYPE_SYSCALL as u32 => BPF_PROG_TYPE_SYSCALL,
-            x if x == BPF_PROG_TYPE_NETFILTER as u32 => BPF_PROG_TYPE_NETFILTER,
+            prog_binding::BPF_PROG_TYPE_CGROUP_SOCKOPT => BPF_PROG_TYPE_CGROUP_SOCKOPT,
+            prog_binding::BPF_PROG_TYPE_TRACING => BPF_PROG_TYPE_TRACING,
+            prog_binding::BPF_PROG_TYPE_STRUCT_OPS => BPF_PROG_TYPE_STRUCT_OPS,
+            prog_binding::BPF_PROG_TYPE_EXT => BPF_PROG_TYPE_EXT,
+            prog_binding::BPF_PROG_TYPE_LSM => BPF_PROG_TYPE_LSM,
+            prog_binding::BPF_PROG_TYPE_SK_LOOKUP => BPF_PROG_TYPE_SK_LOOKUP,
+            prog_binding::BPF_PROG_TYPE_SYSCALL => BPF_PROG_TYPE_SYSCALL,
+            prog_binding::BPF_PROG_TYPE_NETFILTER => BPF_PROG_TYPE_NETFILTER,
             _ => return Err(InvalidTypeBinding { value: prog_type }),
         })
     }
