4098bffe7ce3f288e295a37f842db5a09454ee3f
Tyrone Wu <wudevelops@gmail.com>
2024-10-08 17:54:44 +0000

    aya,aya-obj,test: add PERF_EVENT linkinfo metadata
    Add link metadata for `BPF_LINK_TYPE_PERF_EVENT` with variants: kprobe,
    uprobe, tracepoint, and perf_event.
    
    Additionally add mapping from u32/u64 to FFI:
    - bpf_perf_event_type
    - perf_type_id
    - perf_hw_id
    - perf_sw_ids
    - perf_hw_cache_id
    - perf_hw_cache_op_id
    - perf_hw_cache_op_result_id

--- test/integration-ebpf/src/test.rs
+++ test/integration-ebpf/src/test.rs
@@ -4,12 +4,12 @@
 use aya_ebpf::{
     bindings::xdp_action,
     macros::{
-        cgroup_skb, fentry, kprobe, kretprobe, raw_tracepoint, sk_lookup, tracepoint, uprobe,
-        uretprobe, xdp,
+        cgroup_skb, fentry, kprobe, kretprobe, perf_event, raw_tracepoint, sk_lookup, tracepoint,
+        uprobe, uretprobe, xdp,
     },
     programs::{
-        FEntryContext, ProbeContext, RawTracePointContext, RetProbeContext, SkBuffContext,
-        SkLookupContext, TracePointContext, XdpContext,
+        FEntryContext, PerfEventContext, ProbeContext, RawTracePointContext, RetProbeContext,
+        SkBuffContext, SkLookupContext, TracePointContext, XdpContext,
     },
 };
 
@@ -67,6 +67,11 @@ pub fn cgroup_skb(_ctx: SkBuffContext) -> i32 {
 
 #[sk_lookup]
 pub fn test_sk_lookup(_ctx: SkLookupContext) -> u32 {
+    0
+}
+
+#[perf_event]
+pub fn test_perf_event(_ctx: PerfEventContext) -> u32 {
     0
 }
 
